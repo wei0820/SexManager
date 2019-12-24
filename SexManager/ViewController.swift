594d82fea5be1f4ce0a4c2a32d91c785ae25af23
Wei Wei <jheng.wei0919@gmail.com>
2019-12-24 16:52:59 +0800

    commit

---
+++ SexManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SexManager
+//
+//  Created by oneplay on 2019/12/24.
+//  Copyright © 2019 JackPan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
