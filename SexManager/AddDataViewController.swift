289cd18d6b01e09b97ace7726e4883e60dca79fd
Wei Wei <jheng.wei0919@gmail.com>
2020-01-08 14:38:07 +0800

    add viewcontroller

---
+++ SexManager/AddDataViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddDataViewController.swift
+//  SexManager
+//
+//  Created by oneplay on 2020/1/8.
+//  Copyright © 2020 JackPan. All rights reserved.
+//
+
+import UIKit
+
+class AddDataViewController: mBasicViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
