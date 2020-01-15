dd84820b4003b3af6de39a025e6d5c3f2010d90c
Wei Wei <jheng.wei0919@gmail.com>
2020-01-15 16:10:24 +0800

    add viewcontroller

---
+++ SexManager/SettingCenterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingCenterViewController.swift
+//  SexManager
+//
+//  Created by oneplay on 2020/1/15.
+//  Copyright © 2020 JackPan. All rights reserved.
+//
+
+import UIKit
+
+class SettingCenterViewController: mBasicViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
