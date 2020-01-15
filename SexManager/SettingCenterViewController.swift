6dc55618625938e9cfbe1d7359b832ec830f7280
Wei Wei <jheng.wei0919@gmail.com>
2020-01-15 16:59:48 +0800

    fix view add fun

--- SexManager/SettingCenterViewController.swift
+++ SexManager/SettingCenterViewController.swift
@@ -15,10 +15,22 @@ class SettingCenterViewController: mBasicViewController {
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func addPlace_btn(_ sender: Any) {
+    }
+    @IBAction func addType_btn(_ sender: Any) {
+    }
+    @IBOutlet weak var typeText: UITextField!
+    @IBOutlet weak var typeLabel: UILabel!
     
-
+    @IBAction func exit_btn(_ sender: Any) {
+    }
+    @IBOutlet weak var placeText: UITextField!
+    
+    @IBOutlet weak var placeLabel: UILabel!
     /*
-    // MARK: - Navigation
+     @IBAction func typeText(_ sender: Any) {
+     }
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
