1c06c1432d158b73325d792b39ecfce964e7840d
Wei Wei <jheng.wei0919@gmail.com>
2020-01-21 15:48:32 +0800

    fix view

--- SexManager/SettingCenterViewController.swift
+++ SexManager/SettingCenterViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class SettingCenterViewController: mBasicViewController {
+class SettingCenterViewController: mBasicViewController ,UITextFieldDelegate{
     var placeArray = Array<String>()
     var TypeArray = Array<String>()
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkData()
+        placeText.delegate = self
+        typeText.delegate = self
+        placeText.returnKeyType = .done
+        typeText.returnKeyType = .done
 
         // Do any additional setup after loading the view.
     }
@@ -47,17 +52,46 @@ class SettingCenterViewController: mBasicViewController {
         
         if(userDefaults.array(forKey: "typeArray") != nil){
             var typeNum : Int = userDefaults.array(forKey: "typeArray")!.count
-            
             typeLabel.text = "目前數量：" + String(typeNum)
+        }else{
+            typeLabel.text = "目前數量："
+
         }
         if(userDefaults.array(forKey: "placeArray") != nil ){
             var placenum : Int = userDefaults.array(forKey: "placeArray")!.count
             placeLabel.text = "目前數量：" + String(placenum)
             
+        }else{
+            placeLabel.text = "目前數量："
+
         }
 
         
         
     }
+  //_ textField
+      // 開始進入編輯狀態
+          func textFieldDidBeginEditing(_ textField: UITextField){
+          }
+       
+          // 可能進入結束編輯狀態
+          func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+       
+              return true
+          }
+       
+          // 結束編輯狀態(意指完成輸入或離開焦點)
+          func textFieldDidEndEditing(_ textField: UITextField) {
+          }
+       
+          // 按下Return後會反應的事件
+          func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+              //利用此方式讓按下Return後會Toogle 鍵盤讓它消失
+              textField.resignFirstResponder()
 
+              return false
+          }
+       override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+           self.view.endEditing(true)
+       }
 }
