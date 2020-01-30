887ff928f31314fcdedf40e47690a8c34c763375
Wei Wei <jheng.wei0919@gmail.com>
2020-01-30 18:15:50 +0800

    can get firebase database date type value

--- SexManager/FirebaseManager.swift
+++ SexManager/FirebaseManager.swift
@@ -26,7 +26,8 @@ class FirebaseManager {
                dateReview["Minute"] = min  as AnyObject
                dateReview["Type"] = Type as AnyObject
                 dateReview["Place"] = place as AnyObject
-            dateReview["date"]  = DateManager.getDateforDate() as AnyObject
+            
+        dateReview["date"]  = DateManager.getDateforDate() as AnyObject
            dateReview["createDate"] = DateManager.getDateString2() as AnyObject
            dateReviewReference.updateChildValues(dateReview) { (err, ref) in
                if err != nil{
@@ -51,7 +52,7 @@ class FirebaseManager {
         // childAdded逐筆呈現
                  if let dictionaryData = snapshot.value as? [String: AnyObject]{
                     print("Min",dictionaryData["Minute"])
-                    print("createDate",dictionaryData["createDate"])
+                    print("date",dictionaryData["date"])
                     minarray.append(dictionaryData["Minute"] as! String)
 
                 
